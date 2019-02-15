92c51c4906eb144be7e7ad4d2920aa0c914c1c24
ligang <1055619878@qq.com>
2019-02-15 14:47:26 +0800

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -8,6 +8,6 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).to.include('aaa')
+    expect(wrapper.text()).to.include(msg)
   })
 })
