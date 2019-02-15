ea8e75450ae365560ab13e4bca60d382c771c020
ligang <1055619878@qq.com>
2019-02-15 18:18:42 +0800

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -8,6 +8,6 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).to.include('bbb')
+    expect(wrapper.text()).to.include('qqqq')
   })
 })
